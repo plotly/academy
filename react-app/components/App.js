bd074717f9213fbce6fb59cd9e506319c797a3b7
Max <contact@mstoiber.com>
2016-04-09 15:48:08 +0200

    App third step

--- react-app/components/App.js
+++ react-app/components/App.js
@@ -1,13 +1,31 @@
 var React = require('react');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      city: ''
+    };
+  },
+  fetchData: function(e) {
+    e.preventDefault();
+    console.log('fetch weather for', this.state.city);
+  },
+  changeCity: function(evt) {
+    this.setState({
+      city: evt.target.value
+    });
+  },
   render: function() {
     return (
       <div>
         <h1>Weather</h1>
-        <form>
+        <form onSubmit={this.fetchData}>
           <label>City: </label>
-          <input type="text" />
+          <input
+            value={this.state.city}
+            onChange={this.changeCity}
+            type="text"
+          />
         </form>
       </div>
     );
