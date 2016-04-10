c28842bf980984498093c4d8f9889509aaaab1f7
Max <contact@mstoiber.com>
2016-04-10 11:53:56 +0200

    App fourth step: data fetching

--- react-app/components/App.js
+++ react-app/components/App.js
@@ -1,14 +1,26 @@
 var React = require('react');
+var xhr = require('xhr');
 
 var App = React.createClass({
   getInitialState: function() {
     return {
-      city: ''
+      city: '',
+      data: {}
     };
   },
   fetchData: function(e) {
     e.preventDefault();
-    console.log('fetch weather for', this.state.city);
+    var self = this;
+
+    var location = encodeURIComponent(this.state.city);
+
+    xhr({
+      uri: 'http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&APPID=APPID&units=metric'
+    }, function (err, resp) {
+      self.setState({
+        data: JSON.parse(resp.body)
+      });
+    });
   },
   changeCity: function(evt) {
     this.setState({
@@ -16,17 +28,25 @@ var App = React.createClass({
     });
   },
   render: function() {
+    var currentTemp;
+    if (this.state.data.list && this.state.data.list.length > 0) {
+      currentTemp = this.state.data.list[0].main.temp;
+    }
     return (
       <div>
         <h1>Weather</h1>
         <form onSubmit={this.fetchData}>
-          <label>City: </label>
+          <label>City and Country: </label>
           <input
             value={this.state.city}
             onChange={this.changeCity}
+            placeholder="City, Country"
             type="text"
           />
         </form>
+        <div>
+          { JSON.stringify(currentTemp, null, 2) }
+        </div>
       </div>
     );
   }
