edcaaa26a129347b76f47876ccc92dfc4b4df8cb
Max <contact@mstoiber.com>
2016-07-21 12:09:24 +0200

    Immutable: Step four

--- react-app/components/Plot.js
+++ react-app/components/Plot.js
@@ -2,6 +2,15 @@
 var React = require('react');
 
 var Plot = React.createClass({
+  shouldComponentUpdate: function(prevProps) {
+    var xChanged = !this.props.xData.equals(prevProps.xData);
+    var yChanged = !this.props.yData.equals(prevProps.yData);
+    var typeChanged = this.props.type !== prevProps.type;
+
+    var shouldRender = xChanged || yChanged || typeChanged;
+
+    return shouldRender;
+  },
   drawPlot: function() {
     Plotly.newPlot('plot', [{
       x: this.props.xData.toJS(),
