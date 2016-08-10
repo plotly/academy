3e48c240dbdb7ebd3ab75814afb923b1b80ea919
Max <contact@mstoiber.com>
2016-08-10 08:05:59 +0200

    Fix snapshot testing for Plot

--- weather-app/src/__tests__/components.test.js
+++ weather-app/src/__tests__/components.test.js
@@ -4,17 +4,25 @@ import { fromJS } from 'immutable';
 import { App } from '../App';
 import Plot from '../Plot';
 
-describe('components', () => {
-	describe('<App />', () => {
-		it('renders correctly', () => {
+describe('components', function() {
+	describe('<App />', function() {
+		it('renders correctly', function() {
 			const tree = renderer.create(<App redux={fromJS({})} />).toJSON();
 		  expect(tree).toMatchSnapshot();
 		});
 	});
 
-	describe('<Plot />', () => {
-		it('renders correctly', () => {
-			const tree = renderer.create(<Plot />).toJSON();
+	describe('<Plot />', function() {
+		global.Plotly = {
+			newPlot: function() {}
+		};
+		global.document = {
+			getElementById: function() { return {
+				on: function() {}
+			}}
+		};
+		it('renders correctly', function() {
+			const tree = renderer.create(<Plot xData={fromJS({})} yData={fromJS({})} />).toJSON();
 		  expect(tree).toMatchSnapshot();
 		});
 	});
