80ff70528604319024234bb183a5e2d658f13c80
Max <contact@mstoiber.com>
2016-08-23 19:45:08 +1000

    Remove old react app from repo

--- react-app/constants.js
+++
@@ -1,6 +0,0 @@
-exports.SET_LOCATION = 'weather-app/App/SET_LOCATION';
-exports.SET_DATA = 'weather-app/App/SET_DATA';
-exports.SET_DATES = 'weather-app/App/SET_DATES';
-exports.SET_TEMPS = 'weather-app/App/SET_TEMPS';
-exports.SET_SELECTED_DATE = 'weather-app/App/SET_SELECTED_DATE';
-exports.SET_SELECTED_TEMP = 'weather-app/App/SET_SELECTED_TEMP';
